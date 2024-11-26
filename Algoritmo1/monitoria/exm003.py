58148b18f01be9806056dfba7fde9969b70f99f4
Mika <kakamarques.0202@gmail.com>
2024-11-25 22:12:11 -0300

    Update exm003.py

--- Algoritmo1/monitoria/exm003.py
+++ Algoritmo1/monitoria/exm003.py
@@ -1,7 +1,8 @@
 capital = float(input("Qual o valor do capital a ser investido? "))
 time = int(input("Por quantos meses esse valor será investido? "))
-taxa = 30/100
-montante = capital * ((1 + taxa)**time)
+taxa = float(input("Qual a quantidade da taxa (em decimal)? "))
+montante = capital * (1 + taxa)**time
 
 jurosCompostos = montante - capital
 
+print(f"O valor total de juros é de R${jurosCompostos:,.2f}")
