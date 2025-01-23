00493a85a39c3ec2c7887185b94f4bd974cafee4
Mika <kakamarques.0202@gmail.com>
2025-01-22 22:59:44 -0300

    bug fix

--- Algoritmo1/exercícios/ex028.py
+++ Algoritmo1/exercícios/ex028.py
@@ -1,15 +1,8 @@
-#dá erro caso o ultimo valor da lista seja um pico
-
 def picos(vetor):
     contador = 0
-    for i in range(1, len(vetor)):
-        if vetor[i] <= vetor[i - 1]:
-            continue
-        else:
-            if vetor[i] <= vetor[i + 1]:
-                continue
-            else:
-                contador += 1
+    for i in range(1, len(vetor) - 1):
+        if vetor[i] > vetor[i - 1] and vetor[i] > vetor[i + 1]:
+            contador += 1
     return contador
 
 relevo = [1, 2, 3, -3, -9, 4, 19, 7, 45, 3]
