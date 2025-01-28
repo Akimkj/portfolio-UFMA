7a5e1b1b6c0d9064c89968f5e1cd5ef69b69e183
Mika <kakamarques.0202@gmail.com>
2025-01-28 07:53:34 -0300

    Update desafio001.py

--- Algoritmo1/exercícios/desafio001.py
+++ Algoritmo1/exercícios/desafio001.py
@@ -1,14 +1,17 @@
 # prototipo
 
 def zeroes_ones(v):
-    for i in range(len(v) - 1):
+    for i in range(1, len(v) - 1):
         if v[i] == 0:
             continue
         if v[i] > v[i + 1]:
             v[i], v[i +1] = 0,1
-        
+        if v[i] < v[i - 1]:
+            v[i], v[i - 1] = 1,0
+        if v[i] > v[i + 1]:
+            v[i], v[i + 1] = 0, 1
     return v
 
-vetor = [0, 1, 1, 0, 0, 0, 1]
+vetor = [0, 1, 1, 0, 1, 0, 1]
 
 print(zeroes_ones(vetor))
