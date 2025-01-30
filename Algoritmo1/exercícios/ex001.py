70ca64a19fa21db65ca22374ae9ecd5f56cd6340
Mika <kakamarques.0202@gmail.com>
2025-01-30 10:47:55 -0300

    att monitoria

--- Algoritmo1/exercícios/ex001.py
+++ Algoritmo1/exercícios/ex001.py
@@ -1,1 +1,32 @@
-print("Olá, mundo!")
+v = [-3, 0, 1, 2, 5]              
+
+'''def moveZeroes(nums):
+    for i in nums:
+        if i == 0:
+            nums.append(i)
+            nums.remove(i)
+    return nums'''
+def moveZeroes(nums):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                
+    
+    return nums
+
+print(moveZeroes(v))
+
+def emOrdem(vetor):
+    '''for i in range(len(vetor)):
+        for j in range(i + 1, len(vetor)):
+            if vetor[i] >= vetor[j]:
+                return False'''
+    for i in range(len(vetor) - 1):
+        if vetor[i] >= vetor[i + 1]:
+            return False
+    
+    return True
+
+print(emOrdem(v))
+
--- Algoritmo1/exercícios/ex002.py
+++ Algoritmo1/exercícios/ex002.py
@@ -3,3 +3,15 @@ b = 8
 print(a, b)
 a, b = b, a
 print(a, b)
+'''
+for i in range(7, 1001):
+    num = str(i)
+    if num[-1] == "7":
+        print(i)
+'''
+
+def moveZeroes(nums):
+    for i in nums:
+        if i == 0:
+            nums.append(i)
+            nums.remove(i)
