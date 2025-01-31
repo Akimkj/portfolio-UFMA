53f4c45103c123b706c9edcc932f6559916e107d
Mika <kakamarques.0202@gmail.com>
2025-01-30 22:32:11 -0300

    Update ex037.py

--- Algoritmo1/exercícios/ex037.py
+++ Algoritmo1/exercícios/ex037.py
@@ -1,14 +1,22 @@
 def quartoQuadrante(mat):
-    quadrante = []
+    '''quadrante = []
     for i in range(4, 8):
         linhaQuadrante = []
         for j in range(4, 8):
             linhaQuadrante.append(mat[i][j])
         quadrante.append(linhaQuadrante)
 
-    return quadrante
+    return quadrante'''
+    matriz = []
+    indiceMatriz = 0
+    for i in range(4, 8):
+        matriz.append([])
+        for j in range(4, 8):
+            matriz[indiceMatriz].append(mat[i][j])
+        indiceMatriz += 1
+    return matriz
 
-matriz = [
+m = [
     [1, 2, 3, 4, 5, 6, 7, 8],
     [9, 10, 11, 12, 13, 14, 15, 16],
     [17, 18, 19, 20, 21, 22, 23, 24],
@@ -19,4 +27,4 @@ matriz = [
     [57, 58, 59, 60, 61, 62, 63, 64]
 ]
 
-print(quartoQuadrante(matriz))
+print(quartoQuadrante(m))
