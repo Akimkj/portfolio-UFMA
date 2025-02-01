c36a1aa819a9d2071623b18ba419c3a1dce65c36
Mika <kakamarques.0202@gmail.com>
2025-02-01 19:02:05 -0300

    pygame version 0.11

---
+++ pygame2024.2/main/main.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame.locals import *
+import sys
+
+#Inicializa todas as funções e variaveis da biblioteca pygame
+pygame.init()
+
+# configurações da janela
+largura = 740
+altura = 580
+tela = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption("Space Invaders (Undertale Version)")
+
+
+#Loop principal do jogo
+rodando = True
+while rodando:
+    # Analisa a ocorrencia de algum evento durante a execução
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # botão sair
+            rodando = False
+            pygame.quit()
+            sys.exit()
+    #Atualiza a tela do jogo a cada interação
+    pygame.display.update()
