77874f4ecc2c281dce694f43a78efe098b71ec72
Mika <kakamarques.0202@gmail.com>
2025-02-06 03:01:15 -0300

    Create aprendizado.py

---
+++ pygame2024.2/main/aprendizado.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.locals import *
+import sys
+
+#Inicializa todas as funções e variaveis da biblioteca pygame
+pygame.init()
+
+# configurações da janela
+largura = 740
+altura = 580
+tela = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption("???")
+
+
+#Loop principal do jogo
+rodando = True
+while rodando:
+    # Analisa a ocorrencia de algum evento durante a execução
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # botão sair
+            rodando = False
+            pygame.quit()
+            sys.exit()
+    #Atualiza a tela do jogo a cada interação
+    pygame.display.update()
+
+    #Desenhando retângulo                                     x    y    
+    parede1 = pygame.draw.rect(tela, (255, 0, 0), (370, 290, 40, 40))
+
