14ff22dc2b55b9c40482bbf31935746a022816bd
Mika <kakamarques.0202@gmail.com>
2025-02-19 14:19:37 -0300

    Create colisoes.py

---
+++ pygame2024.2/learn/colisoes.py
@@ -0,0 +1,6 @@
+# Tipos de colisões
+'''
+AABB - desenha os retângulos sobre as sprites para definir as colisões
+CBB - desenha um circulo ao redor da sprite, é mais preciso
+PPC - não desenha figuras geométricas, usa os próprios bits da sprite para identificar a colisão
+'''
