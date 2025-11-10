879e957881d27229874e3fd11e5ac586609e120b
Mika <kakamarques.0202@gmail.com>
2025-11-10 16:57:12 -0300

    feat: implementação do novo TAD lista encadeada

---
+++ ed1/exercicios/sllist.c
@@ -0,0 +1,103 @@
+#ifndef SLLIST_C
+#define SLLIST_C
+
+#include <stdlib.h>
+#include "sllist.h"
+
+typedef struct _slnode_ {
+    void *data;
+    struct _slnode_ *next;
+}SLnode;
+
+typedef struct _sllist_ {
+    SLnode *first;
+}SLlist;
+
+SLlist *sllCreate() {
+    SLlist *l;
+    l = (SLlist *) malloc (sizeof(SLlist));
+    if (l != NULL) {
+        l ->first = NULL;
+        return l;
+    }
+    return NULL;
+}
+
+int sllDestroy(SLlist *l) {
+    if (l != NULL) {
+        if (l->first == NULL) {
+            free(l);
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+int sllInsertAsFirst(SLlist *l, void *data) {
+    SLnode *newnode;
+    if (l != NULL) {
+        newnode = (SLnode*) malloc(sizeof(SLnode));
+        if (newnode != NULL) {
+            newnode->data = data;
+            newnode->next = l->first;
+            l->first = newnode;
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+void *sllRemoveFirst(SLlist *l) {
+    void *data;
+    SLnode *node;
+    if (l != NULL) {
+        if (l->first != NULL) {
+            node = l->first;
+            data = node->data;
+            l->first = node->next;
+            free(node);
+            return data;
+        }
+    }
+    return NULL;
+}
+
+void *sllQueryLast(SLlist *l) {
+    SLnode *last;
+    if (l != NULL) {
+        if (l->first != NULL) {
+            last = l->first;
+            while (last->next != NULL) {
+                last = last->next;
+            }
+            return last->data;
+        }
+    }
+    return NULL;
+}
+
+int sllInsertAsLast(SLlist *l, void *data) {
+    SLnode *newnode;
+    SLnode *last;
+    if (l != NULL) {
+        newnode = (SLnode*) malloc(sizeof(SLnode));
+        if (newnode != NULL) {
+            newnode->data = data;
+            newnode->next = NULL;
+            if (l->first != NULL) {
+                last = l->first;
+                while (last->next != NULL) {
+                    last = last->next;
+                }
+                last->next = newnode;
+            }
+            else {
+                l->first = newnode;
+            }
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+#endif
---
+++ ed1/exercicios/sllist.h
@@ -0,0 +1,23 @@
+#ifndef SLLIST_H
+#define SLLIST_H
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct _slnode_ SLnode;
+typedef struct _sllist_ SLlist;
+
+
+SLlist *sllCreate();
+
+int sllDestroy(SLlist *l);
+
+int sllInsertAsFirst(SLlist *l, void *data);
+
+void *sllRemoveFirst(SLlist *l);
+
+void *sllQueryLast(SLlist *l);
+
+int sllInsertAsLast(SLlist *l, void *data);
+
+#endif
