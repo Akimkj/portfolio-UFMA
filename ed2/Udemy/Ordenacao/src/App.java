027f856b47e7439df3baf742bbc8a8f509ba528d
Mika <kakamarques.0202@gmail.com>
2026-03-07 00:55:29 -0300

    feat: implementação busca binaria

--- ed2/Udemy/Ordenacao/src/App.java
+++ ed2/Udemy/Ordenacao/src/App.java
@@ -42,6 +42,15 @@ public class App {
             } else if (opc == 6) {
                 int high = dados.length -1;
                 quickSort(dados, 0, high);
+            } else if (opc == 7) {
+                System.out.println("Que numero quer procurar?");
+                int key = scn.nextInt();
+                int res = binarySearch(dados, key);
+                if (res != -1) {
+                    System.out.println("ENCONTRADO! Na posição " + res);
+                } else {
+                    System.out.println("Elemento nao encontrado");
+                }
             }
 
         }
@@ -58,7 +67,7 @@ public class App {
         int vetor[] = new int[tam];
 
         for (int i = 0; i < tam; i++) {
-            int num = gerador.nextInt(1000);
+            int num = gerador.nextInt(10000000);
             vetor[i] = num;
         }
 
@@ -189,31 +198,51 @@ public class App {
 
     private static int partition(int[] arr, int low, int high) {
         
-    int pivot = arr[high];
-    int i = low - 1;
+        int pivot = arr[high];
+        int i = low - 1;
 
-    for (int j = low; j <= high -1; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        for (int j = low; j <= high -1; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
+
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i +1;
     }
 
-    int temp = arr[i+1];
-    arr[i+1] = arr[high];
-    arr[high] = temp;
-
-    return i +1;
-}
-
     public static void quickSort(int[] arr, int low, int high) {
-    if (low >= high) return;
+        if (low >= high) 
+            return;
 
-    int p = partition(arr, low, high);
+        int p = partition(arr, low, high);
 
-    quickSort(arr, low, p -1);
-    quickSort(arr, p + 1, high);
-}
+        quickSort(arr, low, p -1);
+        quickSort(arr, p + 1, high);
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
